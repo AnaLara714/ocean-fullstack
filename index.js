f75c876b031978caab821c327d8054b721655592
AnaLara714 <Analara714@gmail.com>
2023-02-10 12:27:38 -0300

    Back-end com banco de dados mongoDB e integração de serviços de back-end na nuvem

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+//localhost ou 127.0.0.1
+// const DB_URL = "mongodb://127.0.0.1:27017";
+const DB_URL = "mongodb+srv://admin:rFQC5GjCRJwwOmnH@cluster0.mbkfjov.mongodb.net";
+const DB_NAME = "ocean-bancodados-09-02-2023"; 
+
+async function main() {
+
+//conexão com o banco de dados
+console.log("Conectando com o banco de dados...");
+const client = await MongoClient.connect(DB_URL);
+const db = client.db(DB_NAME);
+const collection = db.collection("itens");
+console.log("Banco de dados conectado com sucesso!");
+
 const app = express();
 
 //o que vier no body, esta em JSON
@@ -19,23 +34,63 @@ const itensMercado = ["Arroz", "Feijão", "Sabão", "Pão", "Café"];
 //                      0           1       2       3     4
 
 // endpoint Read All -> [GET] /item
-app.get("/item", function (req, res) {
-  res.send(itensMercado);
+app.get("/item", async function (req, res) {
+  const documentos = await collection.find().toArray();
+  res.send(documentos);
 });
 
 // endpoint Read Single by ID -> [GET] /item:id
-app.get("/item/:id", function (req, res) {
+app.get("/item/:id", async function (req, res) {
   const id = req.params.id;
-  const item = itensMercado[id-1];
+  const item = await collection.findOne({ _id: new ObjectId(id) });
   res.send(item);
 });
 
 // endpoint Create -> [POST] /item
-app.post("/item", function (req, res) {
+app.post("/item", async function (req, res) {
   // console.log(req.body)
   const item = req.body;
-  itensMercado.push(item.itemNovo);
-  res.send("item criado com sucesso");
+  await collection.insertOne(item)
+  res.send(item);
 });
 
+// endpoint Update -> [PUT] /item/:id
+//put atuliza tudo, patch atualiza só uma parte
+app.put("/item/:id", async function (req, res) {
+  // console.log(req.body)
+  const id = req.params.id;
+  const body = req.body;
+  // console.log(id, body);
+  await collection.updateOne(
+    {_id: new ObjectId(id)},
+    { $set: body }
+  );
+  res.send("deu bom!");
+});
+
+//Endpoint  Delete - [DELETE] /item/:id
+// exercicio:
+// -> pesquisar
+
 app.listen(3000);
+}
+main(); 
+
+/*    banco de dados U.u
+admin
+rFQC5GjCRJwwOmnH
+mongodb+srv://admin:rFQC5GjCRJwwOmnH@cluster0.mbkfjov.mongodb.net
+*/
+
+/* JSON 
+  object {}
+  {
+    "chave": "valor" | number | boolean | [array] | {object}
+    "array": [
+      "tal"
+    ],
+    object: {
+      0: "tal"
+    }
+  }
+*/
