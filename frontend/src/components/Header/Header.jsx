e07b73a600e3d6bd6c87a48c2bbac568d5ea37e4
AnaLara714 <Analara714@gmail.com>
2023-02-24 16:48:41 -0300

    atualização do conteudo e readById adicionado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <img
-        src='https://cdn-icons-png.flaticon.com/512/4748/4748649.png'
+        src='https://cdn-icons-png.flaticon.com/512/4221/4221927.png'
         width={70} height={70}
       />
-     <h1>Minha lista de Compras</h1>
+     <h1>Minha lista de Filmes e Séries</h1>
       <a href="#" className='button'>Adicionar</a>
+      <a href="#" className='button'>Pesquisar</a>
       <a href="#" className='button'>Home</a>
     </div>
   )
--- frontend/src/components/Tag/Tag.jsx
+++ frontend/src/components/Tag/Tag.jsx
@@ -4,7 +4,7 @@ export default function Tag(props) {
   const tags = props.tag; 
   return (
     <div className="tag">
-      <h5>•{tags}</h5> 
+      <h5>{tags}</h5> 
     </div>
   )
 }
